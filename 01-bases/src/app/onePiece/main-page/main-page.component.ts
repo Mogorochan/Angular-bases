b8596cfb44866c2c9d26dd87f83d987623987262
Natalia <74684373+Mogorochan@users.noreply.github.com>
2022-09-29 16:47:06 -0500

    FormsModule(NgModel) & Two data binding

--- 01-bases/src/app/onePiece/main-page/main-page.component.ts
+++ 01-bases/src/app/onePiece/main-page/main-page.component.ts
@@ -1,12 +1,23 @@
 import { Component} from '@angular/core';
 
+interface Personaje{
+  nombre: string;
+  recompensa: string;
+}
+
 @Component({
   selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  templateUrl: './main-page.component.html'
 })
 export class MainPageComponent{
 
+  nuevo: Personaje = {
+    nombre: 'Sanji',
+    recompensa: '77.000.000 B'
+  }
 
+  agregar(){
+    console.log(this.nuevo);
+  }
 
 }
