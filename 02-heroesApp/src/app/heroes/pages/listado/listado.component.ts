05e6cf2b0869752c6299cbe36e61dbce84c55155
Natalia <74684373+Mogorochan@users.noreply.github.com>
2022-10-04 16:46:01 -0500

    input

--- 02-heroesApp/src/app/heroes/pages/listado/listado.component.ts
+++ 02-heroesApp/src/app/heroes/pages/listado/listado.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-listado',
-  templateUrl: './listado.component.html'
+  templateUrl: './listado.component.html', 
+  styles: [`
+    mat-card{
+      margin-top: 15px
+    }
+  `]
 })
 export class ListadoComponent implements OnInit {
+
+  heroes: Heroe[] = [];
 
   constructor(private HeroesService: HeroesService) { }
 
   ngOnInit(): void {
+    this.HeroesService.getHeroes()
+    .subscribe(lHeroes =>  this.heroes = lHeroes);
   }
 
 }
