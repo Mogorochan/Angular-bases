84c6a69c26405335df25ca2f5fd33cdbebd5180f
Natalia <74684373+Mogorochan@users.noreply.github.com>
2022-10-04 12:07:32 -0500

    servicios

--- 02-heroesApp/src/app/heroes/heroes.module.ts
+++ 02-heroesApp/src/app/heroes/heroes.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
+
 //componentes
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
+
 
 
 
@@ -20,6 +24,8 @@ import { ListadoComponent } from './pages/listado/listado.component';
   ],
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    MaterialModule,
     HeroesRoutingModule
   ]
 })
--- 02-heroesApp/src/app/heroes/pages/listado/listado.component.ts
+++ 02-heroesApp/src/app/heroes/pages/listado/listado.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-listado',
-  templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
+  templateUrl: './listado.component.html'
 })
 export class ListadoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private HeroesService: HeroesService) { }
 
   ngOnInit(): void {
   }
