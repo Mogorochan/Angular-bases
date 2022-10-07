6846abb4da28500d553b782c43d2d5f5c63eff81
Natalia <74684373+Mogorochan@users.noreply.github.com>
2022-10-07 09:33:34 -0500

    ruta del login

--- 02-heroesApp/src/app/auth/auth.module.ts
+++ 02-heroesApp/src/app/auth/auth.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+//creadas
 import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
 //componentes
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
@@ -14,7 +16,8 @@ import { RegistroComponent } from './pages/registro/registro.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule
   ]
 })
 export class AuthModule { }
