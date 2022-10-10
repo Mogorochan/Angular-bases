9f304408a2cbefd3208014451a1a705df35ee55b
Natalia <74684373+Mogorochan@users.noreply.github.com>
2022-10-10 12:04:35 -0500

    librerias y conexión al puerto

--- 03-auth-server/index.js
+++ 03-auth-server/index.js
@@ -1,1 +1,25 @@
-console.log('Hola, mi nombre es Frailejon');
+const express = require('express');
+
+/* 
+todo: Crear el servidor/ app express  */
+const app = express();
+
+/* 
+?Con esto se valida qué responde el servidor*/
+
+app.get('/', (req, res) => {
+    res.json({
+        msg: 'somebody SAAAAAVE me!',
+        saveMeIn: 4200
+    });
+  });
+
+const Port = process.env.Port || 4000
+app.listen(Port, ( ) => {
+    
+    console.log(`Hola, mi nombre es Frailejon corriendo en el puerto ${Port} `);
+})
+
+
+
+
