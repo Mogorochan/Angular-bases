144405828ebf0bfb8b5c89eb3108600d8b993892
Natalia <74684373+Mogorochan@users.noreply.github.com>
2022-10-10 17:14:16 -0500

    name.not().isEmpty()

--- 03-auth-server/routes/auth.routes.js
+++ 03-auth-server/routes/auth.routes.js
@@ -5,7 +5,11 @@ const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/a
 const router = Router();
 
 //crear un nuevo usuario
-router.post('/new', crearUsuario);
+router.post('/new', [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password es obligatorio').isLength({min: 6})
+],crearUsuario);
 
 //login usuario
 router.post('/', [
