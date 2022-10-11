4ec00ab9e2716fcc58b71ca494da450faa4149a0
Natalia <74684373+Mogorochan@users.noreply.github.com>
2022-10-11 15:03:23 -0500

    Grabar usuario enla BD

--- 03-auth-server/controllers/auth.controllers.js
+++ 03-auth-server/controllers/auth.controllers.js
@@ -1,13 +1,48 @@
 const { response} = require('express');
+const Usuario = require('../models/Usuario');
 
-const crearUsuario =  (req, res = response) => {
+
+const crearUsuario =  async(req, res = response) => {
     
     const {name, email, password} = req.body;
 
-    return res.json({
-        ok: true,
-        msg: 'Crear usuario/ new'
-    })
+    try {
+    //Paso1. Verificar sí hay dos email's iguales
+        const usuario = await Usuario.findOne({email}); //verificación
+
+        if (usuario) {
+            return res.status(404).json({
+                ok: false,
+                msg: "El usuario ya existe con ese email, por favos ingresar otro"
+            });
+        }
+
+    //Paso2. Crear usuario con el modelo
+        const dbUser = new Usuario(req.body);
+
+    //Encriptar la contraseña(por medio del hash)
+    //Generar el Jason web token (JWT)
+    //Paso3. Crear usuario en BD
+        await dbUser.save();
+        
+    //Paso4. Generar respuesta exitosa
+        return res.status(201).json({
+            ok: true,
+            uid: dbUser.id,
+            name,
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Algo falló!'
+        });
+    }
+
+
+    
+
 }
 
 const loginUsuario = (req, res = response) => {
