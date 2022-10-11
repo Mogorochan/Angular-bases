27caf5f41dafb6ad01d5dc246faca43cef8f9352
Natalia <74684373+Mogorochan@users.noreply.github.com>
2022-10-11 12:09:55 -0500

    conexión mongoose. Inteto fallido postgres

---
+++ 03-auth-server/db/config.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+// const { Pool, Client} = require('pg');
+// require('dotenv').config();
+
+const dbConnection = async() =>{
+    try {
+        await mongoose.connect(process.env.BD_CNN);
+        console.log('Arriba!');
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error al iniciar la BD');
+    }
+}
+
+// const dbConnection = async() => {
+//     try {
+//         const pool = new Pool({
+//             host: process.env.PGHOST,
+//             user: process.env.PGUSER,
+//             database: process.env.PGDATABASE,
+//             password: process.env.PGPASSWORD,
+//             port: process.env.PGPORT,
+//         });
+ 
+//         await pool.connect()
+//         const res = await pool.query('SELECT * FROM actor')
+//         console.log(res)
+//         await pool.end()
+//     } catch (error) {
+//         console.log(error);
+//         throw new Error('Error al iniciar la BD');
+//     }
+// }
+
+module.exports= {
+    dbConnection
+}
--- 03-auth-server/index.js
+++ 03-auth-server/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('./db/config');
 require('dotenv').config();
 
 /* 
 *Crear el servidor/ app express*/
 const app = express();
+//conexión BD
+dbConnection()
 //Directorio público
 app.use(express.static('public'));
-
 //Cors
 app.use(cors());
 //Lectura y parseo del body
