e7c9c102d956b138ba39c65210b3bb86f8326ff9
Natalia <74684373+Mogorochan@users.noreply.github.com>
2022-10-12 17:59:47 -0500

    exit

--- 03-authApp/src/app/app.module.ts
+++ 03-authApp/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import  {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { ProtectedModule } from './protected/protected.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +13,8 @@ import { ProtectedModule } from './protected/protected.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ProtectedModule
+    HttpClientModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
