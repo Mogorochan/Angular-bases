98c926c853d8208897ecfc1f6ebe44ad4a9b8c52
Natalia <74684373+Mogorochan@users.noreply.github.com>
2022-10-12 16:06:10 -0500

    login  y registro html y validators

--- 03-authApp/src/app/app.module.ts
+++ 03-authApp/src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './protected/dashboard/dashboard.component';
+
+import { ProtectedModule } from './protected/protected.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProtectedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
