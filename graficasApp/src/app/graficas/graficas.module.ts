21e90978e730e69fb9d8dcae5ecf646704d72173
Natalia <74684373+Mogorochan@users.noreply.github.com>
2022-10-14 17:56:18 -0500

    rutas y lazy load

--- graficasApp/src/app/graficas/graficas.module.ts
+++ graficasApp/src/app/graficas/graficas.module.ts
@@ -6,6 +6,7 @@ import { BarrasComponent } from './pages/barras/barras.component';
 import { BarrasDobleComponent } from './pages/barras-doble/barras-doble.component';
 import { DonaComponent } from './pages/dona/dona.component';
 import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.component';
+import { DonaHttpComponent } from './pages/dona-http/dona-http.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.
     BarrasComponent,
     BarrasDobleComponent,
     DonaComponent,
-    GraficaBarraComponent
+    GraficaBarraComponent,
+    DonaHttpComponent
   ],
   imports: [
     CommonModule,
