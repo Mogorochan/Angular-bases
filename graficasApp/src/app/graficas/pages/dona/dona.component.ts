da342693f292035fafb420e8db0db233a91c6365
Natalia <74684373+Mogorochan@users.noreply.github.com>
2022-10-18 16:14:55 -0500

    conexión servidor y petición http postman

--- graficasApp/src/app/graficas/pages/dona/dona.component.ts
+++ graficasApp/src/app/graficas/pages/dona/dona.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component} from '@angular/core';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
 @Component({
   selector: 'app-dona',
   templateUrl: './dona.component.html',
   styles: [
   ]
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent {
 
   constructor() { }
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [ 350, 450, 100 ], 
+        backgroundColor: ['#6405F0','#0724E3', '#05A0F0']
+      },
+ 
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+  
 
-  ngOnInit(): void {
-  }
 
 }
