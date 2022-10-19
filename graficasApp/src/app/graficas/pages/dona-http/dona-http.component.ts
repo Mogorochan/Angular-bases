0c77fd797b63c17b2e9096a5e71b6dba10387200
Natalia <74684373+Mogorochan@users.noreply.github.com>
2022-10-19 09:10:05 -0500

    fin donachart

--- graficasApp/src/app/graficas/pages/dona-http/dona-http.component.ts
+++ graficasApp/src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GraficasService } from '../../services/graficas.service';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
+
 
 @Component({
   selector: 'app-dona-http',
@@ -9,13 +11,38 @@ import { GraficasService } from '../../services/graficas.service';
 })
 export class DonaHttpComponent implements OnInit {
 
+  
+  public doughnutChartLabels: string[] = [ ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [ ], 
+        backgroundColor: ['#6405F0','#0724E3', '#05A0F0']
+      },
+      
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+  
   constructor(private graficasService: GraficasService) { }
 
-  ngOnInit(): void {
-    this.graficasService.getDataDona()
-    .subscribe(data =>{
-     console.log(data);
-    })
-  }
+ngOnInit(): void {
+  // this.graficasService.getDataDona()
+  // .subscribe(resp =>{
+  //  console.log(resp);
+  //  const labels=Object.keys(resp)
+  //  const datos=Object.values(resp)
+  //  this.doughnutChartData={
+  //   labels:Object.keys(resp),
+  //   datasets:[{data:Object.values(resp)}]
+  // }
+  // })
+  this.graficasService
+  .getUsuariosRedesSocialesDonaData()
+  .subscribe( ({labels, datasets }) => {
+    this.doughnutChartData = { labels, datasets};
+  })
 
 }
+}
+
--- graficasApp/src/app/graficas/services/graficas.service.ts
+++ graficasApp/src/app/graficas/services/graficas.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {map, delay} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,18 @@ export class GraficasService {
 
   getDataDona(){
     return this.http.get('http://localhost:3000/grafica');
+  }
+
+  getUsuariosRedesSocialesDonaData() {
+    return this.getDataDona()
+    .pipe(
+      delay(1500),
+      map((data) => {
+        return {
+          labels: Object.keys(data),
+          datasets: [{ data: Object.values(data) }],
+        };
+      })
+    );
   }
 }
